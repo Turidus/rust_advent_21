d76bc1107700c25a34af8a955969d8d442d35e4a
LSF <>
2021-12-01 20:04:50 +0100

    Initial commit

---
+++ src/day_one.rs
@@ -0,0 +1,52 @@
+pub mod puzzle_one {
+    use std::fs;
+    use std::path::Path;
+
+
+    pub fn run(path: &Path) {
+        let input = fs::read_to_string(path)
+            .expect("Reading the file was not possible.");
+
+        let lines = input.lines();
+
+        let mut acc = 0;
+        let mut before = i32::MAX;
+
+        for line in lines {
+            let num: i32 = line.trim().parse().expect("Line was not a number");
+            if num > before {acc+=1};
+            before = num;
+        }
+        println!("The result is: {}", acc)
+    }
+}
+
+pub mod puzzle_two {
+    use std::fs;
+    use std::path::Path;
+
+    pub fn run(path: &Path){
+        let input = fs::read_to_string(path)
+            .expect("Reading the file was not possible.");
+
+        let lines = input.lines();
+
+        let mut numbers: Vec<i32> = Vec::new();
+        for line in lines {
+             numbers.push(line.trim().parse().expect("Line was not a number"));
+        }
+        let mut slidingAverages: Vec<i32> = Vec::new();
+        for i in 0..numbers.len()-2 {
+            slidingAverages.push(numbers[i] + numbers[i+1] + numbers[i+2])
+        }
+
+        let mut acc = 0;
+        let mut before = i32::MAX;
+
+        for num in slidingAverages {
+            if num > before {acc+=1};
+            before = num;
+        }
+        println!("The result is: {}", acc)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod day_one;
+
+/*#[macro_use] extern crate lazy_static;*/
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let usage = String::from("Usage: rust_advent[.exe] day:int puzzle:int fileName:String. Example: rust_advent.exe 1 2 1_2");
+    println!("{:?}", args);
+
+    let day: u8 = args.get(1).expect(&*usage).parse().expect(&*usage);
+
+    if day < 1 || day > 24 {panic!("{}", usage)}
+
+    let puzzle: u8 = args.get(2).expect(&*usage).parse().expect(&*usage);
+
+    if puzzle < 1  || puzzle > 2 {panic!("{}", usage)}
+
+    let f_name = args.get(3).expect(&*usage);
+
+    let base = env::current_dir().unwrap();
+    let path= match base.parent(){
+        None => {base.join("inputFiles").join(f_name)}
+        Some(parent) => {parent.join("inputFiles").join(f_name)}
+    };
+
+    match day {
+        1 => {
+            if puzzle == 1 {
+                day_one::puzzle_one::run(&*path)
+            }
+            else {
+                day_one::puzzle_two::run(&*path)
+            }
+        }
+        _ => {panic!("{}", usage)}
+    }
 }
